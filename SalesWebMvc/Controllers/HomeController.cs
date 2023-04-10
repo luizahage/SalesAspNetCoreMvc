7e839cfe3c7265f64ad3b5a629da75d4ff9396da
Luiza Hage <luiza.t.hage@gmail.com>
2023-04-10 07:25:00 -0300

    First Model-Controller-View - Department

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebMvc.Models;
+using SalesWebMvc.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,7 +17,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
             ViewData["Aluna"] = "Luiza Teixeira Hage Firme";
 
             return View();
